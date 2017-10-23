e31d56c2e8022c4ea06f3d9b2ba6389420eaf7c8
Marc <marc@fullstackmarc.com>
2017-10-23 16:29:28 +0200

    Restructure

---
+++ tests/csvx/test_csv_file.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+""" Test for CsvFile class in csvx module """
+import unittest
+from csvx.csv_file import CsvFile
+
+class CsvFileTestSuite(unittest.TestCase):
+    """ CsvFile test cases. """
+
+
+    def test_fail_open_empty_path(self):
+        """ should fail when initialized with an empty string """
+        with self.assertRaises(IOError):
+            CsvFile('')
+
+    def test_get_rows_for_columns(self):
+        """ should get all rows only with the given columns and its values """
+        path = 'tests/resources/test_csv_a.csv'
+        csv_file = CsvFile(path)
+        rows = csv_file.get_rows_for_columns(['column2', 'price'])
+        self.assertGreater(len(rows), 0)
+
+if __name__ == '__main__':
+    unittest.main()
